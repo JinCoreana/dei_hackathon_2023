c648d316aa9d93343dfffbe00b45417cbe2b9d0c
Jin Choi <jin.choi@sage.com>
2023-06-20 13:00:42 +0200

    feat: enable carbon design token

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CarbonProvider from "carbon-react/lib/components/carbon-provider";
+import { sageTheme } from "carbon-react/lib/style/themes";
 import HomePage from "./pages/homePage/HomePage";
 import AccommodationRequestPage from "./pages/accommodationRequest/AccommodationRequestPage";
+import "carbon-react/lib/style/fonts.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/accommodation_request"
-          element={<AccommodationRequestPage />}
-        />
-      </Routes>
-    </Router>
+    <CarbonProvider theme={sageTheme} validationRedesignOptIn>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/accommodation_request"
+            element={<AccommodationRequestPage />}
+          />
+        </Routes>
+      </Router>
+    </CarbonProvider>
   </React.StrictMode>,
   document.getElementById("root") as HTMLElement
 );
