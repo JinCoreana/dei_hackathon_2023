5b3b430237b9e3019482c80d0395971f5a517898
aryan-walia <aryan.walia@sage.com>
2023-06-21 15:26:18 +0530

    Video Carousel Added

---
+++ src/pages/homePage/components/VideoCarousel/VideoCarousel.tsx
@@ -0,0 +1,28 @@
+import Box from "carbon-react/lib/components/box/box.component";
+import { GridItem } from "carbon-react/lib/components/grid";
+import { ReactElement } from "react";
+import { Carousel } from "carbon-react/lib/components/carousel";
+import { videosForCarousel } from "./videos/videos";
+import VideoCard from "./components/VideoCard";
+import SectionTitle from "../../../../globalComponents/SectionTitle";
+
+const VideoCarousel = (): ReactElement => {
+  return (
+    <GridItem
+      alignSelf="stretch"
+      justifySelf="stretch"
+      align-items="center"
+      justify-items="center"
+    >
+      <Box>
+        <SectionTitle title="Success Stories" />
+        <Carousel enableSlideSelector={false}>
+          {videosForCarousel.map((video) => {
+            return <VideoCard url={video.url} title={video.title} />;
+          })}
+        </Carousel>
+      </Box>
+    </GridItem>
+  );
+};
+export default VideoCarousel;
