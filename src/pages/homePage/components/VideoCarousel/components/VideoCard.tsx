06d0f5a93d617980ca0f840f7f64e6f01c39ac48
aryan-walia <aryan.walia@sage.com>
2023-06-21 15:26:18 +0530

    feat: video carousel implemented

---
+++ src/pages/homePage/components/VideoCarousel/components/VideoCard.tsx
@@ -0,0 +1,24 @@
+import Box from "carbon-react/lib/components/box/box.component";
+import Typography from "carbon-react/lib/components/typography/typography.component";
+
+export interface VideoCardProps {
+  url: string;
+  title: string;
+}
+
+const VideoCard = ({ url, title }: VideoCardProps) => {
+  return (
+    <Box mr={130} ml={-100}>
+      <iframe
+        className="videoFrame"
+        frameBorder={0}
+        height={300}
+        width={500}
+        src={url}
+        allowFullScreen
+      ></iframe>
+      <Typography fontSize="20px">{title}</Typography>
+    </Box>
+  );
+};
+export default VideoCard;
