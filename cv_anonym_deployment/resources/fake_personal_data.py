73f4d6111abb2df9bdb99aa441535ca98b46dfda
Bona Chow <bona.chow@sage.com>
2023-06-21 14:56:37 +0100

    add fake CVs and requirements.txt

---
+++ cv_anonym_deployment/resources/fake_personal_data.py
@@ -0,0 +1,83 @@
+fake_personal_data1 = {
+    "data": {
+        "id": 1,
+        "full_name": "Bona Chow",
+        "first_name": "Bona",
+        "last_name": "Chow",
+        "email": "fakeemail@gmail.com",
+        "address": "54 Long Street, Anytown, XP9 8JQ",
+        "summary": None,
+        "phone_number": "07777888999",
+        "source": "referral",
+        "created_at": "2020-09-27T11:05:03Z",
+        "disqualified_date": "2020-10-28",
+        "hired_date": None,
+        "stage": {},
+        "position_id": 1,
+        "added_by": {},
+        "referrer": {}
+    }
+}
+
+fake_personal_data2 = {
+    "data": {
+        "id": 2,
+        "full_name": "Umesh Gopal",
+        "first_name": "Umesh",
+        "last_name": "Gopal",
+        "email": "umesh.gopal@zetymail.in",
+        "address": "",
+        "summary": None,
+        "phone_number": "088 90970695",
+        "source": "referral",
+        "created_at": "2020-10-21T12:58:43Z",
+        "disqualified_date": "2020-10-28",
+        "hired_date": None,
+        "stage": {},
+        "position_id": 1,
+        "added_by": {},
+        "referrer": {}
+    }
+}
+
+fake_personal_data3 = {
+    "data": {
+        "id": 3,
+        "full_name": "John Smith",
+        "first_name": "John",
+        "last_name": "Smith",
+        "email": "johnsmith@example.com",
+        "address": "123 Main Street, Anytown, USA",
+        "summary": None,
+        "phone_number": "(555) 123-4567",
+        "source": "referral",
+        "created_at": "2020-11-20T13:51:13Z",
+        "disqualified_date": "2020-11-29",
+        "hired_date": None,
+        "stage": {},
+        "position_id": 1,
+        "added_by": {},
+        "referrer": {}
+    }
+}
+
+fake_personal_data4 = {
+    "data": {
+        "id": 4,
+        "full_name": "Ellie Howell",
+        "first_name": "Ellie",
+        "last_name": "Howell",
+        "email": "elliehowell@example.com",
+        "address": "25 High Street, Somerset, UK",
+        "summary": None,
+        "phone_number": "+44 1234 567890",
+        "source": "referral",
+        "created_at": "2020-10-15T09:51:23Z",
+        "disqualified_date": "2020-10-28",
+        "hired_date": None,
+        "stage": {},
+        "position_id": 1,
+        "added_by": {},
+        "referrer": {}
+    }
+}
