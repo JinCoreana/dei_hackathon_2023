1be99d919c08e2b9f4a972b0e4811459f5055557
aryan-walia <aryan.walia@sage.com>
2023-06-27 00:53:23 +0530

    my request cards implemented with requests and no requests view

--- src/pages/adjustmentRequest/components/MyRequest.tsx
+++ src/pages/adjustmentRequest/components/MyRequest.tsx
@@ -18,104 +18,129 @@ import Icon from "carbon-react/lib/components/icon";
 import Link from "carbon-react/lib/components/link";
 import Pill from "carbon-react/lib/components/pill";
 const MyRequest = (): ReactElement => {
-  const navigateTo = useNavigate();
 
-  const chatToUs = [
+  // for demo purpose, uncomment line 24 and comment the other const myRequest (mock data would be used otherwise)
+  
+  // const myRequests: any[] = [];
+  const myRequests: any[] = [
     {
-      bgColor: "#007E45",
-      iconImage: chatIcon,
-      title: "Chat to us",
-      subtitle: "(Any questions we can help with)",
-      href: "/chat_with_us",
+      heading: "Visual Support",
+      status: { heading: "Status ", value: "Approved", pillBorder:"#008A21"},
+      requested: { heading: "Requested:", value: "07/05/2023" },
+      details: { heading: "Detail:", value: "Closed captioning" },
     },
     {
-      bgColor: "#007E45",
-      iconImage: chatIcon,
-      title: "Chat to us",
-      subtitle: "(Any questions we can help with)",
-      href: "/chat_with_us",
+      heading: "Quiet or private space",
+      status: { heading: "Status ", value: "Declined", pillBorder:"#CB374A"},
+      requested: { heading: "Requested: ", value: "06/05/2023" },
+      details: { heading: "Detail:", value: "N/A" },
     },
     {
-      bgColor: "#007E45",
-      iconImage: chatIcon,
-      title: "Chat to us",
-      subtitle: "(Any questions we can help with)",
-      href: "/chat_with_us",
+      heading: "Additional time",
+      status: { heading: "Status ", value: "In review", pillBorder:"#335B70"},
+      requested: { heading: "Requested:", value: "02/05/2023" },
+      details: { heading: "Detail:", value: "Regular breaks" },
     },
   ];
 
   return (
     <>
-      <GridItem alignSelf="stretch" justifySelf="stretch" gridColumn="1/13">
-        <SectionTitle title="Interview adjustments" />
-        <Typography
-          variant="h3"
-          alignItems="left"
-          ml={100}
-          mb={20}
-          fontSize="28px"
-        >
-          You have submitted the following adjustment requests:
-        </Typography>
-        <Typography
-          fontWeight="strong"
-          alignItems="left"
-          fontSize="24px"
-          ml={100}
-          mb={50}
-        >
-          To see more details about your requests, go to your notifications.
-        </Typography>
+      {myRequests.length > 0 ? (
+        <GridItem alignSelf="stretch" justifySelf="stretch" gridColumn="1/13">
+          <SectionTitle title="My adjustments" />
+          <Typography
+            variant="h3"
+            alignItems="left"
+            ml={100}
+            mb={20}
+            fontSize="28px"
+          >
+            You have submitted the following adjustment requests:
+          </Typography>
+          <Typography
+            fontWeight="strong"
+            alignItems="left"
+            fontSize="24px"
+            ml={100}
+            mb={50}
+          >
+            To see more details about your requests, go to your notifications.
+          </Typography>
 
-        <Box display="flex" justifyContent="center" flexWrap="wrap">
-          {chatToUs.map((item) => {
-            return (
-              <>
-                <Card
-                  spacing="small"
-                  boxShadow="boxShadow050"
-                  cardWidth="350px"
-                  height="190px"
-                >
-                  <CardRow>
-                    <Typography variant="h3" alignItems="left" m={0}>
-                      user.name@sage.com
-                    </Typography>
-                  </CardRow>
-                  <CardRow>
-                    <Typography fontSize="16px" m={0}>
-                      user.name@sage.com
-                    </Typography>
-
-                    <Pill>default pill</Pill>
-                  </CardRow>
-                  <CardRow>
-                    <Typography fontSize="16px" m={0}>
-                      user.name@sage.com
-                    </Typography>
-                  </CardRow>
-                  <CardRow>
-                    <Typography fontSize="16px" m={0}>
-                      user.name@sage.com
-                    </Typography>
-                  </CardRow>
-                </Card>
-              </>
-            );
-          })}
-        </Box>
-        <Typography
-          variant="p"
-          alignItems="left"
-          fontSize="24px"
-          ml={100}
-          mb={50}
-        >
-          If you need to change anything about your requests or have any
-          questions, get in touch with us below.
-        </Typography>
-      </GridItem>
+          <Box display="flex" justifyContent="center" flexWrap="wrap">
+            {myRequests.map((item) => {
+              return (
+                <>
+                  <Card
+                    spacing="small"
+                    boxShadow="boxShadow100"
+                    cardWidth="350px"
+                    height="auto"
+                    roundness="default"
+                  >
+                    <CardRow>
+                      <Typography variant="segment-header" alignItems="center" mt={2}>
+                        {item.heading}
+                      </Typography>
+                    </CardRow>
+                    <CardRow>
+                      <Typography fontSize="20px" fontWeight="bold" mr={2}>
+                        {item.status.heading}
+                      </Typography>
+                      <Pill borderColor={item.status.pillBorder}>{item.status.value}</Pill>
+                    </CardRow>
+                    <CardRow>
+                      <Typography fontSize="20px" fontWeight="bold" m={0}>
+                        {item.requested.heading}
+                      </Typography>
+                      <Typography variant="h4" m={0}>
+                        {item.requested.value}
+                      </Typography>
+                    </CardRow>
+                    <CardRow>
+                      <Typography fontSize="20px" fontWeight="bold" m={0}>
+                        {item.details.heading}
+                      </Typography>
+                      <Typography variant="h4" mb={2}>
+                        {item.details.value}
+                      </Typography>
+                    </CardRow>
+                  </Card>
+                </>
+              );
+            })
+            }
+          </Box>          
+          <Typography
+            variant="p"
+            alignItems="left"
+            fontSize="24px"
+            ml={100}
+            mb={50}
+          >
+            If you need to change anything about your requests or have any
+            questions, get in touch with us below.
+          </Typography>
+        </GridItem>
+      ) :
+        (   
+          <GridItem alignSelf="stretch" justifySelf="stretch" gridColumn="1/13">
+          <SectionTitle title="My adjustments" />
+          <Typography
+            variant="h3"
+            alignItems="left"
+            ml={100}
+            mb={20}
+            fontSize="28px"
+          >
+            You have not made any adjustment requests yet.
+          </Typography>
+          </GridItem>
+        )
+      
+      }
     </>
+      
   );
 };
 
