a9ccb968908d7b7cb72e5add9cc361f7c20894b3
Martí Baldi <marti.baldi@sage.com>
2023-06-26 19:49:07 +0200

    Chat icon name inconsistency fixed

--- src/pages/adjustmentRequest/components/ChatWithUs.tsx
+++ src/pages/adjustmentRequest/components/ChatWithUs.tsx
@@ -5,7 +5,7 @@ import Typography from "carbon-react/lib/components/typography/typography.compon
 import { ReactElement } from "react";
 import { useNavigate } from "react-router-dom";
 import Image from "carbon-react/lib/components/image";
-import chatIcon from "../../../assets/images/MicrosoftTeams-image (16) 3.png";
+import chatIcon from "../../../assets/images/chat.png";
 const ChatWithUs = (): ReactElement => {
   const navigateTo = useNavigate();
 
