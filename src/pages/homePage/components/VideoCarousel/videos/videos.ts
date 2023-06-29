97bd1ebb6d5f61e2874a9b3f24479d8ca1b42f97
Gauravkhetwal0 <gaurav.khetwal@sage.com>
2023-06-29 13:13:25 +0530

    Changed captions in video carousel.

--- src/pages/homePage/components/VideoCarousel/videos/videos.ts
+++ src/pages/homePage/components/VideoCarousel/videos/videos.ts
@@ -8,13 +8,11 @@ export const videosForCarousel: Array<VideoCardProps> = [
   },
   {
     url: "https://player.vimeo.com/video/772034517?h=c08596b578&title=0&byline=0&portrait=0",
-    title:
-      "Elisa Moscolin, Chief People Officer, Sustainibility & Society Executive Sponsor",
+    title: "Elisa Moscolin, EVP, Sage Foundation & Sustainibility",
   },
   {
     url: "https://player.vimeo.com/video/772034034?h=46f960584c&title=0&byline=0&portrait=0",
-    title:
-      "Steve Hare, Chief People Officer, Sustainibility & Society Executive Sponsor",
+    title: "Steve Hare, Chief Executive Officer, Sage",
   },
   {
     url: "https://player.vimeo.com/video/772034312?h=041fdb7692&title=0&byline=0&portrait=0",
@@ -23,18 +21,15 @@ export const videosForCarousel: Array<VideoCardProps> = [
   },
   {
     url: "https://player.vimeo.com/video/772034517?h=c08596b578&title=0&byline=0&portrait=0",
-    title:
-      "Elisa Moscolin, Chief People Officer, Sustainibility & Society Executive Sponsor",
+    title: "Elisa Moscolin, EVP, Sage Foundation & Sustainability",
   },
   {
     url: "https://player.vimeo.com/video/772034034?h=46f960584c&title=0&byline=0&portrait=0",
-    title:
-      "Steve Hare, Chief People Officer, Sustainibility & Society Executive Sponsor",
+    title: "Steve Hare,Chief Executive Officer, Sage",
   },
   {
     url: "https://player.vimeo.com/video/772034517?h=c08596b578&title=0&byline=0&portrait=0",
-    title:
-      "Elisa Moscolin, Chief People Officer, Sustainibility & Society Executive Sponsor",
+    title: "Elisa Moscolin,EVP, Sage Foundation & Sustainability",
   },
   {
     url: "https://player.vimeo.com/video/772034312?h=041fdb7692&title=0&byline=0&portrait=0",
@@ -43,12 +38,10 @@ export const videosForCarousel: Array<VideoCardProps> = [
   },
   {
     url: "https://player.vimeo.com/video/772034517?h=c08596b578&title=0&byline=0&portrait=0",
-    title:
-      "Elisa Moscolin, Chief People Officer, Sustainibility & Society Executive Sponsor",
+    title: "Elisa Moscolin, EVP, Sage Foundation & Sustainability",
   },
   {
     url: "https://player.vimeo.com/video/772034034?h=46f960584c&title=0&byline=0&portrait=0",
-    title:
-      "Steve Hare, Chief People Officer, Sustainibility & Society Executive Sponsor",
+    title: "Steve Hare, Chief Executive Officer, Sage",
   },
 ];
